9eabfdfdb37e73a242eb7c933474d7b0ee4de7fc
Furkan <kvkfurkan@hotmail.com>
2024-09-11 23:21:58 +0300

    Bottom navigation added

--- app/src/main/java/dev/furkankavak/pocketwords/MainActivity.kt
+++ app/src/main/java/dev/furkankavak/pocketwords/MainActivity.kt
@@ -2,15 +2,24 @@ package dev.furkankavak.pocketwords
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import dev.furkankavak.pocketwords.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private lateinit var navHost : NavHostFragment
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNav.setupWithNavController(navController)
     }
 }
